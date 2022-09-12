8482ec515ec92657a1926bcea87f69522a2ccfb9
lilsara <111012075+lilsara@users.noreply.github.com>
2022-09-11 20:21:59 -0400

    Add files via upload

--- java.js
+++ java.js
@@ -8,22 +8,12 @@ let myDog = {
 myDog.mySound = "I bark loud";
 window.alert ("Hello, my name is " + myDog.name +", when I bark " + myDog.mySound + ". I starred in the tv show " +myDog.tvProgram + ". My character was a " + myDog.breed + ". I was " + myDog.notes + ".");
 
-function myDog2(){
-'name' : 'Echo',
-'mySound': "people get scared",
-'breed': "Belgian Malinois",
-'tvProgram':"Dog",
-'notes':"I'm in the military"
-'canTalk':"yes"
+function myDog3(name, mySound, breed, tvProgram, notes){
+this.name= name;
+this.mySound = mySound;
+this.breed= breed;
+this.tvProgram= tvProgram;
+this.notes= notes;
 }
-
-function myDog3(){
-this.name= "Echo";
-this.mySound = "people get scared";
-this.breed= "Belgian Malinois";
-this.tvProgram= "Dog";
-this.notes= "I'm in the military.";
-}
-let myDog2= new myDog3("");
+let myDog2= new myDog3("Echo", "people get scared", "Belgian Malinois", "Dog", "a military dog");
 document.write("Hello, my name is " + myDog2.name +", when I bark " + myDog2.mySound + ". I starred in the tv show " +myDog2.tvProgram + ". My character was a " + myDog2.breed + ". I was " + myDog2.notes + ".");
-alert(Object.keys(myDog2));
